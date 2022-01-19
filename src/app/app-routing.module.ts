50c1b57e4e316ff4beb2dc40bbb194bc8f41dc95
Derick Augusto <dehhrick@gmail.com>
2022-01-19 00:35:16 -0300

    implementacao da tela de cadastro de equipamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EquipamentoComponent } from './cadastro/equipamento/equipamento.component';
 import { HomeComponent } from './home/home.component';
 import { CriarContaComponent } from './usuario/login/criar-conta/criar-conta.component';
 import { EsqueciMinhaSenhaComponent } from './usuario/login/esqueci-minha-senha/esqueci-minha-senha.component';
@@ -8,7 +10,12 @@ import { LoginComponent } from './usuario/login/login.component';
 const routes: Routes = [
 {path:'', component:LoginComponent },
 {path:'esqueci-minha-senha', component:EsqueciMinhaSenhaComponent},
-{path:'criar-conta', component:CriarContaComponent}
+{path:'criar-conta', component:CriarContaComponent},
+{path:'home', component:HomeComponent},
+{path:'cadastro', component:CadastroComponent, children:[
+  {path:'', redirectTo:'equipamento', pathMatch:'full'},
+  {path:'equipamento', component:EquipamentoComponent}
+]}
 ];
 
 @NgModule({
